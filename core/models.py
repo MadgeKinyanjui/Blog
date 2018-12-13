bb121bbf3f314d1784f777bf93d33cd79ba5b8eb
MadgeKinyanjui <margaret.kinyanjui@andela.com>
2018-12-13 12:17:38 +0300

    Remodel

--- core/models.py
+++ core/models.py
@@ -1,9 +1,14 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
     category_name = models.CharField(max_length=200)
     article_number = models.IntegerField()
+
+    def __str__(self):
+        return self.category_name
 
 
 class Article(models.Model):
@@ -12,6 +17,13 @@ class Article(models.Model):
     content = models.CharField(max_length=200)
     writer_name = models.IntegerField(default=0)
     category_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.tittle
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 #
 #
 # # class Writer(models.Model):
